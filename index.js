566e85927cb2e3306fec80f014551baa81682dc1
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2020-09-03 14:42:43 -0400

    Updated socket.io and removed node_modules

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ console.log("runningg");
 let location = '/public';
 let express = require('express');
 let app = express();
-let serv = require('http').Server(app);
+let serv = require('http').createServer(app);
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + location + '/index.html');
 });
 app.use(location, express.static(__dirname + location));
 let port = process.env.PORT || 8080;
 serv.listen(port);
+
 
 let SOCKET_LIST = {};
 let socket = require('socket.io');
